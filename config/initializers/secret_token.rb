7fa53e955c25179f60ded3ccf6271e1029be4b44
Katie Turner <katherineseniaturner@gmail.com>
2013-05-26 22:36:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrilliantJewels::Application.config.secret_token = 'b2b09b7409e2ef163ca066ad0f1eda700381ef4cd48359910b19aa62c1ba58000658c7b63b30bec8e2f1661e4ba2cd52ec13f0eaf96caccc7debae1848edfb13'
